0b7f1adde4fabd4050993ae5a99cddb7fbd08813
Aleksey Sugonyako <91505310+aleksgtd@users.noreply.github.com>
2022-02-07 16:27:07 +0200

    finishes task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,37 @@
+// Напиши скрипт управления формой логина.
 
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля,
+//     выводи alert с предупреждением о том,
+//         что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму,
+//     собери значения полей в обьект,
+//     где имя поля будет именем свойства,
+//     а значение поля - значением свойства.
+//         Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и
+// очисти значения полей формы методом reset.
+
+const formEl = document.querySelector('form.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    if (
+        event.currentTarget.elements.email.value !== '' &&
+        event.currentTarget.elements.password.value !== ''
+    ) {
+        const newObj = { email: 0, password: 0 };
+
+        newObj.email = event.currentTarget.elements.email.value;
+        newObj.password = event.currentTarget.elements.password.value;
+
+        console.log(newObj);
+        formEl.reset();
+    } else {
+        alert(`Необходимо заполнить все поля`);
+    }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const spanForColorName = document.querySelector('span.color');
+const changeColorBtn = document.querySelector('button.change-color');
+
+changeColorBtn.addEventListener('click', onChangeBgColor);
+
+const bodyEl = document.querySelector('body');
+
+function onChangeBgColor(event) {
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+    spanForColorName.textContent = getRandomHexColor();
+
+    bodyEl.style.backgroundColor = getRandomHexColor();
 }
